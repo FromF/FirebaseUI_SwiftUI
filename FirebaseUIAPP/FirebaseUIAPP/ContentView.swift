9ee9fc8eefaaa21f1ca8ad217c7e88103b0b22c1
Haruhito Fuji <haruhito.fuji@gmail.com>
2020-08-28 17:15:39 +0900

    プロジェクト作成

---
+++ FirebaseUIAPP/FirebaseUIAPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirebaseUIAPP
+//
+//  Created by 藤 治仁 on 2020/08/28.
+//  Copyright © 2020 FromF.github.com. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
