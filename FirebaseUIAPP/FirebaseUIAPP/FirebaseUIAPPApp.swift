be985a51f6458974ac641f72fdc51ffbec4ce3bb
Haruhito Fuji <haruhito.fuji@gmail.com>
2023-07-12 00:34:07 +0900

    ライフサイクルをUIKitからSwiftUI

---
+++ FirebaseUIAPP/FirebaseUIAPP/FirebaseUIAPPApp.swift
@@ -0,0 +1,40 @@
+//
+//  FirebaseUIAPPApp.swift
+//  FirebaseUIAPP
+//
+//  Created by 藤治仁 on 2023/07/12.
+//  Copyright © 2023 FromF.github.com. All rights reserved.
+//
+
+import SwiftUI
+import FirebaseCore
+import FirebaseAuthUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
+        return true
+    }
+    // MARK: URL Schemes
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+        let sourceApplication = options[UIApplication.OpenURLOptionsKey.sourceApplication] as! String?
+        if FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication) ?? false {
+            return true
+        }
+        // other URL handling goes here.
+        return false
+    }
+}
+
+@main
+struct ChatAPPApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
